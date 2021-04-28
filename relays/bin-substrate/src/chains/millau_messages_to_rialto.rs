f4c2c4856c129e32a8a98024a5784b745c54c250
fewensa <fewensa@protonmail.com>
2021-04-28 18:15:16 +0800

    test code

--- relays/bin-substrate/src/chains/millau_messages_to_rialto.rs
+++ relays/bin-substrate/src/chains/millau_messages_to_rialto.rs
@@ -71,7 +71,7 @@ impl SubstrateMessageLane for MillauMessagesToRialto {
 	) -> Bytes {
 		let (relayers_state, proof) = proof;
 		let call: millau_runtime::Call =
-			millau_runtime::MessagesCall::receive_messages_delivery_proof(proof, relayers_state).into();
+			millau_runtime::MessagesCall::receive_messages_delivery_proof::<millau_runtime::WithRialtoMessagesInstance>(proof, relayers_state).into();
 		let call_weight = call.get_dispatch_info().weight;
 		let genesis_hash = *self.source_client.genesis_hash();
 		let transaction = Millau::sign_transaction(genesis_hash, &self.source_sign, transaction_nonce, call);
