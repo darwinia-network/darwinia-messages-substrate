55731eaaac565ca5b3d28074aab610915e95baf0
ouwenkg <2630582710@qq.com>
2021-09-07 18:06:21 +0800

    Add fee market frame work

---
+++ modules/fee-market/src/lib.rs
@@ -0,0 +1,355 @@
+// This file is part of Darwinia.
+//
+// Copyright (C) 2018-2021 Darwinia Network
+// SPDX-License-Identifier: GPL-3.0
+//
+// Darwinia is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Darwinia is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Darwinia. If not, see <https://www.gnu.org/licenses/>.
+
+//! # Fee Market Module
+
+#![cfg_attr(not(feature = "std"), no_std)]
+#![recursion_limit = "128"]
+
+#[cfg(test)]
+mod tests;
+
+pub mod weights;
+use crate::weights::WeightInfo;
+
+use codec::{Decode, Encode};
+use darwinia_support::balance::{LockFor, LockableCurrency};
+use frame_support::{
+	dispatch::DispatchError,
+	ensure,
+	pallet_prelude::*,
+	traits::{Currency, Get, LockIdentifier, WithdrawReasons},
+	transactional, PalletId,
+};
+use frame_system::{ensure_signed, pallet_prelude::*};
+use sp_std::{
+	cmp::{Ord, Ordering},
+	default::Default,
+	vec::Vec,
+};
+
+pub type AccountId<T> = <T as frame_system::Config>::AccountId;
+pub type RingBalance<T> = <<T as Config>::RingCurrency as Currency<AccountId<T>>>::Balance;
+pub type Fee<T> = RingBalance<T>;
+
+pub use pallet::*;
+
+#[frame_support::pallet]
+pub mod pallet {
+	use super::*;
+
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		#[pallet::constant]
+		type PalletId: Get<PalletId>;
+		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+
+		#[pallet::constant]
+		type MiniumLockValue: Get<RingBalance<Self>>;
+		#[pallet::constant]
+		type MinimumFee: Get<Fee<Self>>;
+		#[pallet::constant]
+		type PriorRelayersNumber: Get<u64>;
+		#[pallet::constant]
+		type LockId: Get<LockIdentifier>;
+
+		type RingCurrency: LockableCurrency<Self::AccountId, Moment = Self::BlockNumber>;
+		type WeightInfo: WeightInfo;
+	}
+
+	#[pallet::event]
+	#[pallet::generate_deposit(fn deposit_event)]
+	#[pallet::metadata(T::AccountId = "AccountId")]
+	pub enum Event<T: Config> {
+		/// Relayer register
+		Register(T::AccountId, RingBalance<T>, Fee<T>),
+		/// Update relayer lock balance
+		UpdateLockedBalance(T::AccountId, RingBalance<T>),
+		/// Update relayer fee
+		UpdateFee(T::AccountId, Fee<T>),
+		/// Cancel relayer register
+		CancelRelayerRegister(T::AccountId),
+	}
+
+	#[pallet::error]
+	pub enum Error<T> {
+		/// Insufficient balance
+		InsufficientBalance,
+		/// The lock value is lower than MiniumLockLimit
+		TooLowLockValue,
+		/// The relayer has been registered
+		AlreadyRegistered,
+		/// Register before update lock value
+		RegisterBeforeUpdateLock,
+		/// Invalid new lock value
+		InvalidNewLockValue,
+		/// Only Relayer can submit fee
+		InvalidSubmitPriceOrigin,
+		/// The fee is lower than MinimumFee
+		TooLowFee,
+	}
+
+	#[pallet::storage]
+	#[pallet::getter(fn get_relayer)]
+	pub type RelayersMap<T: Config> =
+		StorageMap<_, Blake2_128Concat, T::AccountId, Relayer<T>, ValueQuery>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn relayers)]
+	pub type Relayers<T: Config> = StorageValue<_, Vec<T::AccountId>, ValueQuery>;
+
+	/// The lowest n fees, p.0 < p.1 < p.2 ... < p.n
+	#[pallet::storage]
+	#[pallet::getter(fn prior_relayers)]
+	pub type PriorRelayers<T: Config> = StorageValue<_, Vec<(T::AccountId, Fee<T>)>, ValueQuery>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn best_relayer)]
+	pub type TopRelayer<T: Config> = StorageValue<_, (T::AccountId, Fee<T>), ValueQuery>;
+
+	#[pallet::genesis_config]
+	pub struct GenesisConfig {}
+	#[cfg(feature = "std")]
+	impl Default for GenesisConfig {
+		fn default() -> Self {
+			Self {}
+		}
+	}
+	#[pallet::genesis_build]
+	impl<T: Config> GenesisBuild<T> for GenesisConfig {
+		fn build(&self) {}
+	}
+
+	#[pallet::pallet]
+	pub struct Pallet<T>(_);
+	#[pallet::hooks]
+	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
+
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+		/// Register to be a relayer
+		#[pallet::weight(10000)]
+		#[transactional]
+		pub fn register(
+			origin: OriginFor<T>,
+			lock_value: RingBalance<T>,
+			fee: Option<Fee<T>>,
+		) -> DispatchResultWithPostInfo {
+			let who = ensure_signed(origin)?;
+			ensure!(
+				lock_value >= T::MiniumLockValue::get(),
+				<Error<T>>::TooLowLockValue
+			);
+			ensure!(
+				T::RingCurrency::free_balance(&who) >= lock_value,
+				<Error<T>>::InsufficientBalance
+			);
+			ensure!(!Self::is_registered(&who), <Error<T>>::AlreadyRegistered);
+			if let Some(p) = fee {
+				ensure!(p >= T::MinimumFee::get(), <Error<T>>::TooLowFee);
+			}
+
+			let fee = fee.unwrap_or_else(T::MinimumFee::get);
+			T::RingCurrency::set_lock(
+				T::LockId::get(),
+				&who,
+				LockFor::Common { amount: lock_value },
+				WithdrawReasons::all(),
+			);
+
+			<RelayersMap<T>>::insert(&who, Relayer::new(who.clone(), lock_value, fee));
+			<Relayers<T>>::append(who.clone());
+
+			Self::update_relayer_fees()?;
+			Self::deposit_event(Event::<T>::Register(who, lock_value, fee));
+			Ok(().into())
+		}
+
+		/// Relayer update locked balance
+		#[pallet::weight(10000)]
+		#[transactional]
+		pub fn update_locked_balance(
+			origin: OriginFor<T>,
+			new_lock: RingBalance<T>,
+		) -> DispatchResultWithPostInfo {
+			let who = ensure_signed(origin)?;
+			ensure!(
+				Self::is_registered(&who),
+				<Error<T>>::RegisterBeforeUpdateLock
+			);
+			ensure!(
+				T::RingCurrency::free_balance(&who) >= new_lock,
+				<Error<T>>::InsufficientBalance
+			);
+			ensure!(
+				new_lock > Self::get_relayer(&who).lock_balance,
+				<Error<T>>::InvalidNewLockValue
+			);
+
+			T::RingCurrency::extend_lock(T::LockId::get(), &who, new_lock, WithdrawReasons::all())?;
+			<RelayersMap<T>>::mutate(who.clone(), |relayer| {
+				relayer.lock_balance = new_lock;
+			});
+			Self::deposit_event(Event::<T>::UpdateLockedBalance(who, new_lock));
+			Ok(().into())
+		}
+
+		/// Relayer cancel register
+		#[pallet::weight(10000)]
+		#[transactional]
+		pub fn cancel_register(origin: OriginFor<T>) -> DispatchResultWithPostInfo {
+			let who = ensure_signed(origin)?;
+			ensure!(
+				Self::is_registered(&who),
+				<Error<T>>::RegisterBeforeUpdateLock
+			);
+
+			T::RingCurrency::remove_lock(T::LockId::get(), &who);
+			RelayersMap::<T>::remove(who.clone());
+			Relayers::<T>::mutate(|relayers| relayers.retain(|x| x != &who));
+
+			Self::update_relayer_fees()?;
+			Self::deposit_event(Event::<T>::CancelRelayerRegister(who));
+			Ok(().into())
+		}
+
+		/// Relayer update fee
+		#[pallet::weight(10000)]
+		#[transactional]
+		pub fn update_fee(origin: OriginFor<T>, p: Fee<T>) -> DispatchResultWithPostInfo {
+			let who = ensure_signed(origin)?;
+			ensure!(
+				Self::is_registered(&who),
+				<Error<T>>::InvalidSubmitPriceOrigin
+			);
+			ensure!(p >= T::MinimumFee::get(), <Error<T>>::TooLowFee);
+
+			<RelayersMap<T>>::mutate(who.clone(), |relayer| {
+				relayer.fee = p;
+			});
+
+			Self::update_relayer_fees()?;
+			Self::deposit_event(Event::<T>::UpdateFee(who, p));
+			Ok(().into())
+		}
+	}
+}
+
+impl<T: Config> Pallet<T> {
+	/// Update fees in the following cases:
+	/// 1. New relayer register
+	/// 2. Already registered relayer update fee
+	/// 3. Cancel registered relayer
+	pub fn update_relayer_fees() -> Result<(), DispatchError> {
+		<PriorRelayers<T>>::kill();
+
+		let mut relayers: Vec<Relayer<T>> = <Relayers<T>>::get()
+			.iter()
+			.map(RelayersMap::<T>::get)
+			.collect();
+		relayers.sort();
+
+		// If the registered relayers number >= the PriorRelayersNumber,
+		// append the lowest PriorRelayersNumber relayers to PriorRelayers and choose the last one as TopRelayer.
+		if relayers.len() >= T::PriorRelayersNumber::get() as usize {
+			for i in 0..T::PriorRelayersNumber::get() as usize {
+				let r = &relayers[i];
+				<PriorRelayers<T>>::append((r.id.clone(), r.fee));
+			}
+		} else {
+			// If the registered relayers number < the PriorRelayersNumber,
+			// append all submit fee to PriorRelayers and choose the last one as TopRelayer
+			for r in relayers.iter() {
+				<PriorRelayers<T>>::append((r.id.clone(), r.fee));
+			}
+		}
+		<TopRelayer<T>>::put(
+			<PriorRelayers<T>>::get()
+				.iter()
+				.last()
+				.map(|(r, p)| ((*r).clone(), *p))
+				.unwrap_or_default(),
+		);
+		Ok(())
+	}
+
+	/// Whether the relayer has registered
+	pub fn is_registered(who: &T::AccountId) -> bool {
+		<Relayers<T>>::get().iter().any(|r| *r == *who)
+	}
+
+	// Get relayer fee
+	pub fn relayer_price(who: &T::AccountId) -> Fee<T> {
+		Self::get_relayer(who).fee
+	}
+
+	// Get relayer locked balance
+	pub fn relayer_locked_balance(who: &T::AccountId) -> RingBalance<T> {
+		Self::get_relayer(who).lock_balance
+	}
+
+	pub fn slash_relayer() {
+		// slash relayers
+		// if the lock ring lower than limit, remove it auto
+		todo!()
+	}
+}
+#[derive(Encode, Decode, Clone, Eq, Debug)]
+pub struct Relayer<T: Config> {
+	id: T::AccountId,
+	lock_balance: RingBalance<T>,
+	fee: Fee<T>,
+}
+
+impl<T: Config> Relayer<T> {
+	pub fn new(id: T::AccountId, lock_balance: RingBalance<T>, fee: Fee<T>) -> Relayer<T> {
+		Relayer {
+			id,
+			lock_balance,
+			fee,
+		}
+	}
+}
+
+impl<T: Config> PartialOrd for Relayer<T> {
+	fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+		self.fee.partial_cmp(&other.fee)
+	}
+}
+
+impl<T: Config> Ord for Relayer<T> {
+	fn cmp(&self, other: &Self) -> Ordering {
+		self.fee.cmp(&other.fee)
+	}
+}
+
+impl<T: Config> PartialEq for Relayer<T> {
+	fn eq(&self, other: &Self) -> bool {
+		self.fee == other.fee && self.id == other.id && self.lock_balance == other.lock_balance
+	}
+}
+
+impl<T: Config> Default for Relayer<T> {
+	fn default() -> Self {
+		Relayer {
+			id: T::AccountId::default(),
+			lock_balance: RingBalance::<T>::default(),
+			fee: Fee::<T>::default(),
+		}
+	}
+}
---
+++ modules/fee-market/src/test.rs
@@ -0,0 +1,399 @@
+// This file is part of Darwinia.
+//
+// Copyright (C) 2018-2021 Darwinia Network
+// SPDX-License-Identifier: GPL-3.0
+//
+// Darwinia is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Darwinia is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Darwinia. If not, see <https://www.gnu.org/licenses/>.
+
+// --- substrate ---
+use frame_support::{
+	assert_err, assert_ok,
+	traits::{GenesisBuild, LockIdentifier},
+	PalletId,
+};
+use frame_system::mocking::*;
+use sp_core::H256;
+use sp_runtime::{
+	testing::Header,
+	traits::{BlakeTwo256, IdentityLookup},
+	RuntimeDebug,
+};
+// --- darwinia ---
+use crate::{self as darwinia_fee_market, *};
+
+type Block = MockBlock<Test>;
+type UncheckedExtrinsic = MockUncheckedExtrinsic<Test>;
+type Balance = u64;
+
+darwinia_support::impl_test_account_data! {}
+
+impl frame_system::Config for Test {
+	type BaseCallFilter = ();
+	type BlockWeights = ();
+	type BlockLength = ();
+	type DbWeight = ();
+	type Origin = Origin;
+	type Index = u64;
+	type BlockNumber = u64;
+	type Hash = H256;
+	type Call = Call;
+	type Hashing = BlakeTwo256;
+	type AccountId = u64;
+	type Lookup = IdentityLookup<Self::AccountId>;
+	type Header = Header;
+	type Event = Event;
+	type BlockHashCount = ();
+	type Version = ();
+	type PalletInfo = PalletInfo;
+	type AccountData = AccountData<Balance>;
+	type OnNewAccount = ();
+	type OnKilledAccount = ();
+	type SystemWeightInfo = ();
+	type SS58Prefix = ();
+	type OnSetCode = ();
+}
+
+frame_support::parameter_types! {
+	pub const ExistentialDeposit: u64 = 1;
+}
+impl darwinia_balances::Config<RingInstance> for Test {
+	type Balance = Balance;
+	type DustRemoval = ();
+	type Event = Event;
+	type ExistentialDeposit = ExistentialDeposit;
+	type BalanceInfo = AccountData<Balance>;
+	type AccountStore = System;
+	type MaxLocks = ();
+	type OtherCurrencies = ();
+	type WeightInfo = ();
+}
+
+frame_support::parameter_types! {
+	pub const MinimumPeriod: u64 = 1000;
+}
+impl pallet_timestamp::Config for Test {
+	type Moment = u64;
+	type OnTimestampSet = ();
+	type MinimumPeriod = MinimumPeriod;
+	type WeightInfo = ();
+}
+
+frame_support::parameter_types! {
+	pub const FeeMarketPalletId: PalletId = PalletId(*b"da/feemk");
+	pub const FeeMarketLockId: LockIdentifier = *b"da/feelf";
+	pub const MiniumLockValue: Balance = 2;
+	pub const MinimumFee: Balance = 2;
+	pub const PriorRelayersNumber: u64 = 3;
+}
+
+impl Config for Test {
+	type PalletId = FeeMarketPalletId;
+	type Event = Event;
+	type MiniumLockValue = MiniumLockValue;
+	type MinimumFee = MinimumFee;
+	type PriorRelayersNumber = PriorRelayersNumber;
+	type LockId = FeeMarketLockId;
+	type RingCurrency = Ring;
+	type WeightInfo = ();
+}
+
+frame_support::construct_runtime! {
+	pub enum Test where
+		Block = Block,
+		NodeBlock = Block,
+		UncheckedExtrinsic = UncheckedExtrinsic,
+	{
+		System: frame_system::{Pallet, Call, Config, Storage, Event<T>},
+		Timestamp: pallet_timestamp::{Pallet, Call, Storage},
+		Ring: darwinia_balances::<Instance1>::{Pallet, Call, Storage, Config<T>, Event<T>},
+		FeeMarket: darwinia_fee_market::{Pallet, Call, Storage, Config, Event<T>},
+	}
+}
+pub fn new_test_ext() -> sp_io::TestExternalities {
+	let mut t = frame_system::GenesisConfig::default()
+		.build_storage::<Test>()
+		.unwrap();
+	darwinia_balances::GenesisConfig::<Test, RingInstance> {
+		balances: vec![(1, 10), (2, 20), (3, 30), (4, 40), (5, 50), (12, 10)],
+	}
+	.assimilate_storage(&mut t)
+	.unwrap();
+
+	let mut ext = sp_io::TestExternalities::new(t);
+	ext.execute_with(|| System::set_block_number(1));
+	ext
+}
+
+#[test]
+fn test_register_workflow_works() {
+	new_test_ext().execute_with(|| {
+		assert_eq!(Ring::free_balance(1), 10);
+		assert_err!(
+			FeeMarket::register(Origin::signed(1), 1, None),
+			<Error<Test>>::TooLowLockValue
+		);
+		assert_err!(
+			FeeMarket::register(Origin::signed(1), 50, None),
+			<Error<Test>>::InsufficientBalance
+		);
+
+		assert_ok!(FeeMarket::register(Origin::signed(1), 5, None));
+		assert!(FeeMarket::is_registered(&1));
+		assert_eq!(FeeMarket::relayers().len(), 1);
+		assert_eq!(Ring::usable_balance(&1), 5);
+		assert_eq!(FeeMarket::relayer_locked_balance(&1), 5);
+		assert_eq!(FeeMarket::best_relayer(), (1, 2));
+
+		assert_err!(
+			FeeMarket::register(Origin::signed(1), 5, None),
+			<Error<Test>>::AlreadyRegistered
+		);
+	});
+}
+
+#[test]
+fn test_relayer_register_update_price() {
+	new_test_ext().execute_with(|| {
+		assert_ok!(FeeMarket::register(Origin::signed(1), 5, Some(10)));
+		assert_ok!(FeeMarket::register(Origin::signed(2), 5, Some(11)));
+		assert_ok!(FeeMarket::register(Origin::signed(3), 5, Some(12)));
+		assert_ok!(FeeMarket::register(Origin::signed(4), 5, Some(13)));
+
+		assert_eq!(FeeMarket::relayers(), vec![1, 2, 3, 4]);
+		assert_eq!(FeeMarket::prior_relayers().len(), 3);
+		assert_eq!(FeeMarket::best_relayer(), (3, 12));
+	});
+}
+
+#[test]
+fn test_update_locked_balance_success() {
+	new_test_ext().execute_with(|| {
+		assert_err!(
+			FeeMarket::update_locked_balance(Origin::signed(1), 5),
+			<Error::<Test>>::RegisterBeforeUpdateLock
+		);
+		assert_ok!(FeeMarket::register(Origin::signed(1), 5, None));
+		assert!(FeeMarket::is_registered(&1));
+
+		// update lock balance from 5 to 8
+		assert_ok!(FeeMarket::update_locked_balance(Origin::signed(1), 8));
+		assert_eq!(Ring::usable_balance(&1), 2);
+		assert_eq!(FeeMarket::relayer_locked_balance(&1), 8);
+		assert_eq!(FeeMarket::best_relayer(), (1, 2));
+	});
+}
+
+#[test]
+fn test_update_locked_balance_failed() {
+	new_test_ext().execute_with(|| {
+		assert_ok!(FeeMarket::register(Origin::signed(1), 5, None));
+
+		// update lock balance from 5 to 8
+		assert_ok!(FeeMarket::update_locked_balance(Origin::signed(1), 8));
+		// update lock balance from 8 to 8
+		assert_err!(
+			FeeMarket::update_locked_balance(Origin::signed(1), 3),
+			<Error<Test>>::InvalidNewLockValue
+		);
+		// update lock balance from 8 to 3
+		assert_err!(
+			FeeMarket::update_locked_balance(Origin::signed(1), 3),
+			<Error<Test>>::InvalidNewLockValue
+		);
+		assert_eq!(Ring::usable_balance(&1), 2);
+		assert_eq!(FeeMarket::relayer_locked_balance(&1), 8);
+	});
+}
+
+#[test]
+fn test_cancel_register() {
+	new_test_ext().execute_with(|| {
+		assert_err!(
+			FeeMarket::cancel_register(Origin::signed(1)),
+			<Error<Test>>::RegisterBeforeUpdateLock
+		);
+
+		assert_ok!(FeeMarket::register(Origin::signed(1), 5, None));
+		assert!(FeeMarket::is_registered(&1));
+		assert_eq!(Ring::usable_balance(&1), 5);
+		assert_eq!(FeeMarket::relayer_locked_balance(&1), 5);
+
+		assert_ok!(FeeMarket::cancel_register(Origin::signed(1)));
+		assert_eq!(FeeMarket::relayer_locked_balance(&1), 0);
+		assert!(!FeeMarket::is_registered(&1));
+	});
+}
+
+#[test]
+fn test_cancel_register_and_update_price() {
+	new_test_ext().execute_with(|| {
+		assert_ok!(FeeMarket::register(Origin::signed(1), 5, None));
+		assert_ok!(FeeMarket::register(Origin::signed(2), 5, None));
+		assert_ok!(FeeMarket::register(Origin::signed(3), 5, None));
+		assert_ok!(FeeMarket::register(Origin::signed(4), 5, None));
+		assert_ok!(FeeMarket::register(Origin::signed(5), 5, None));
+		assert_eq!(FeeMarket::relayers(), vec![1, 2, 3, 4, 5]);
+		assert_eq!(FeeMarket::prior_relayers()[0], (1, 2));
+		assert_eq!(FeeMarket::prior_relayers()[1], (2, 2));
+		assert_eq!(FeeMarket::prior_relayers()[2], (3, 2));
+		assert_eq!(FeeMarket::best_relayer(), (3, 2));
+
+		assert_ok!(FeeMarket::cancel_register(Origin::signed(1)));
+		assert_ok!(FeeMarket::cancel_register(Origin::signed(5)));
+		assert!(!FeeMarket::is_registered(&1));
+		assert!(!FeeMarket::is_registered(&5));
+		assert_eq!(FeeMarket::relayers(), vec![2, 3, 4]);
+		assert_eq!(FeeMarket::prior_relayers()[0], (2, 2));
+		assert_eq!(FeeMarket::prior_relayers()[1], (3, 2));
+		assert_eq!(FeeMarket::prior_relayers()[2], (4, 2));
+		assert_eq!(FeeMarket::best_relayer(), (4, 2));
+	});
+}
+
+#[test]
+fn test_locked_ring_list_works() {
+	new_test_ext().execute_with(|| {
+		assert_ok!(FeeMarket::register(Origin::signed(1), 5, None));
+		assert_ok!(FeeMarket::register(Origin::signed(2), 10, None));
+		assert_ok!(FeeMarket::register(Origin::signed(3), 15, None));
+		assert_ok!(FeeMarket::register(Origin::signed(4), 20, None));
+
+		assert_eq!(FeeMarket::relayer_locked_balance(&1), 5);
+		assert_eq!(FeeMarket::relayer_locked_balance(&2), 10);
+		assert_eq!(FeeMarket::relayer_locked_balance(&3), 15);
+		assert_eq!(FeeMarket::relayer_locked_balance(&4), 20);
+
+		assert_ok!(FeeMarket::update_locked_balance(Origin::signed(1), 6));
+		assert_ok!(FeeMarket::update_locked_balance(Origin::signed(2), 11));
+		assert_ok!(FeeMarket::update_locked_balance(Origin::signed(3), 16));
+		assert_ok!(FeeMarket::update_locked_balance(Origin::signed(4), 21));
+
+		assert_eq!(FeeMarket::relayer_locked_balance(&1), 6);
+		assert_eq!(FeeMarket::relayer_locked_balance(&2), 11);
+		assert_eq!(FeeMarket::relayer_locked_balance(&3), 16);
+		assert_eq!(FeeMarket::relayer_locked_balance(&4), 21);
+
+		assert_ok!(FeeMarket::cancel_register(Origin::signed(1)));
+		assert_ok!(FeeMarket::cancel_register(Origin::signed(2)));
+		assert_ok!(FeeMarket::cancel_register(Origin::signed(3)));
+		assert_ok!(FeeMarket::cancel_register(Origin::signed(4)));
+
+		assert_eq!(FeeMarket::relayer_locked_balance(&1), 0);
+		assert_eq!(FeeMarket::relayer_locked_balance(&2), 0);
+		assert_eq!(FeeMarket::relayer_locked_balance(&3), 0);
+		assert_eq!(FeeMarket::relayer_locked_balance(&4), 0);
+	});
+}
+
+#[test]
+fn test_update_price_basic_storage_works() {
+	new_test_ext().execute_with(|| {
+		assert_ok!(FeeMarket::register(Origin::signed(1), 5, None));
+		assert_err!(
+			FeeMarket::update_fee(Origin::signed(1), 1),
+			<Error<Test>>::TooLowFee
+		);
+
+		assert_ok!(FeeMarket::update_fee(Origin::signed(1), 2));
+		assert_eq!(FeeMarket::relayer_price(&1), 2);
+		assert_eq!(FeeMarket::relayers(), vec![1]);
+	});
+}
+
+#[test]
+fn test_few_relayer_duplicate_update_one_price() {
+	new_test_ext().execute_with(|| {
+		assert_ok!(FeeMarket::register(Origin::signed(1), 5, None));
+		assert_ok!(FeeMarket::update_fee(Origin::signed(1), 2));
+		assert_ok!(FeeMarket::update_fee(Origin::signed(1), 2));
+
+		assert_eq!(FeeMarket::relayers(), vec![1]);
+		assert_eq!(FeeMarket::prior_relayers()[0], (1, 2));
+		assert_eq!(FeeMarket::prior_relayers().len(), 1);
+		assert_eq!(FeeMarket::best_relayer(), (1, 2));
+	});
+}
+
+#[test]
+fn test_few_relayer_update_one_price() {
+	new_test_ext().execute_with(|| {
+		assert_ok!(FeeMarket::register(Origin::signed(1), 5, None));
+		assert_ok!(FeeMarket::register(Origin::signed(2), 5, None));
+		assert_ok!(FeeMarket::update_fee(Origin::signed(1), 4));
+		assert_ok!(FeeMarket::update_fee(Origin::signed(2), 4));
+
+		assert_eq!(FeeMarket::prior_relayers()[0], (1, 4));
+		assert_eq!(FeeMarket::prior_relayers()[1], (2, 4));
+		assert_eq!(FeeMarket::prior_relayers().len(), 2);
+		assert_eq!(FeeMarket::best_relayer(), (2, 4));
+	});
+}
+
+#[test]
+fn test_few_relayer_update_more_price() {
+	new_test_ext().execute_with(|| {
+		assert_ok!(FeeMarket::register(Origin::signed(1), 5, None));
+		assert_ok!(FeeMarket::register(Origin::signed(2), 5, None));
+		assert_ok!(FeeMarket::update_fee(Origin::signed(1), 2));
+		assert_ok!(FeeMarket::update_fee(Origin::signed(2), 3));
+
+		assert_eq!(FeeMarket::relayers(), vec![1, 2]);
+		assert_eq!(FeeMarket::prior_relayers()[0], (1, 2));
+		assert_eq!(FeeMarket::prior_relayers()[1], (2, 3));
+		assert_eq!(FeeMarket::prior_relayers().len(), 2);
+		assert_eq!(FeeMarket::best_relayer(), (2, 3));
+	});
+}
+
+#[test]
+fn test_mul_relayer_update_one_price() {
+	new_test_ext().execute_with(|| {
+		assert_ok!(FeeMarket::register(Origin::signed(1), 5, None));
+		assert_ok!(FeeMarket::register(Origin::signed(2), 5, None));
+		assert_ok!(FeeMarket::register(Origin::signed(3), 5, None));
+		assert_ok!(FeeMarket::register(Origin::signed(4), 5, None));
+		assert_ok!(FeeMarket::update_fee(Origin::signed(1), 10));
+		assert_ok!(FeeMarket::update_fee(Origin::signed(2), 10));
+		assert_ok!(FeeMarket::update_fee(Origin::signed(3), 10));
+		assert_ok!(FeeMarket::update_fee(Origin::signed(4), 10));
+
+		assert_eq!(FeeMarket::relayers(), vec![1, 2, 3, 4]);
+		assert_eq!(FeeMarket::prior_relayers().len(), 3);
+		assert_eq!(FeeMarket::prior_relayers()[0], (1, 10));
+		assert_eq!(FeeMarket::prior_relayers()[1], (2, 10));
+		assert_eq!(FeeMarket::prior_relayers()[2], (3, 10));
+		assert_eq!(FeeMarket::best_relayer(), (3, 10));
+	});
+}
+
+#[test]
+fn test_mul_relayer_update_diff_price() {
+	new_test_ext().execute_with(|| {
+		assert_ok!(FeeMarket::register(Origin::signed(1), 5, None));
+		assert_ok!(FeeMarket::register(Origin::signed(2), 5, None));
+		assert_ok!(FeeMarket::register(Origin::signed(3), 5, None));
+		assert_ok!(FeeMarket::register(Origin::signed(4), 5, None));
+		assert_ok!(FeeMarket::update_fee(Origin::signed(1), 10));
+		assert_ok!(FeeMarket::update_fee(Origin::signed(2), 20));
+		assert_ok!(FeeMarket::update_fee(Origin::signed(3), 30));
+		assert_ok!(FeeMarket::update_fee(Origin::signed(4), 40));
+
+		assert_eq!(FeeMarket::relayers(), vec![1, 2, 3, 4]);
+		assert_eq!(FeeMarket::prior_relayers().len(), 3);
+		assert_eq!(FeeMarket::prior_relayers()[0], (1, 10));
+		assert_eq!(FeeMarket::prior_relayers()[1], (2, 20));
+		assert_eq!(FeeMarket::prior_relayers()[2], (3, 30));
+		assert_eq!(FeeMarket::best_relayer(), (3, 30));
+	});
+}
