2915bb9d0ad7b959bbd060a2464d4e2b4e431d17
nada <37610161+furoxr@users.noreply.github.com>
2022-03-30 16:37:05 +0800

    Update pangolin parachain id

--- primitives/runtime/src/lib.rs
+++ primitives/runtime/src/lib.rs
@@ -67,7 +67,7 @@ pub const PANGORO_CHAIN_ID: ChainId = *b"pagr";
 pub const PANGOLIN_CHAIN_ID: ChainId = *b"pagl";
 
 /// Bridge-with-PangolinParachain instance id.
-pub const PANGOLIN_PARACHAIN_CHAIN_ID: ChainId = *b"ppal";
+pub const PANGOLIN_PARACHAIN_CHAIN_ID: ChainId = *b"pglp";
 
 /// Call-dispatch module prefix.
 pub const CALL_DISPATCH_MODULE_PREFIX: &[u8] = b"pallet-bridge/dispatch";
