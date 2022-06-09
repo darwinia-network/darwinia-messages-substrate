ea3adffb71321ca0aefe20b437597526c579e6e5
Guantong <04637@163.com>
2022-06-09 10:25:08 +0800

    Add Crab & Crab parachain

---
+++ bin/runtime-common/src/lanes.rs
@@ -0,0 +1,13 @@
+use bp_messages::LaneId;
+
+/// Identifier of bridge between Darwinia and Crab.
+pub const DARWINIA_CRAB_LANE: LaneId = [0; 4];
+
+// Identifier of bridge between Pangoro and Pangolin.
+pub const PANGORO_PANGOLIN_LANE: LaneId = *b"roli";
+
+/// Identifier of bridge between Pangolin and Pangolin Parachain.
+pub const PANGOLIN_PANGOLIN_PARACHAIN_LANE: LaneId = *b"pali";
+
+/// Identifier of bridge between Crab and Crab Parachain.
+pub const CRAB_CRAB_PARACHAIN_LANE: LaneId = *b"pacr";
