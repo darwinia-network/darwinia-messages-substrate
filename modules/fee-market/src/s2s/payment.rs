fd3fde7958819f6f0ba89b317de9d2160458f115
bear <boundless.forest@outlook.com>
2022-06-16 10:24:33 +0800

    `Pre-dispatch` validate for main branch (#130)
    * Adjust the traits
    
    * Try fix compile
    
    * Fix tests
    
    * Code clean

--- modules/fee-market/src/s2s/payment.rs
+++ modules/fee-market/src/s2s/payment.rs
@@ -25,6 +25,7 @@ use frame_support::{
 	log,
 	traits::{Currency as CurrencyT, ExistenceRequirement, Get},
 };
+use scale_info::TypeInfo;
 use sp_runtime::traits::{AccountIdConversion, Saturating, Zero};
 use sp_std::{
 	collections::{btree_map::BTreeMap, vec_deque::VecDeque},
