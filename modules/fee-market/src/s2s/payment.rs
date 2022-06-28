b94d89c2c132aa13143e04a1eb52e9b1fe041843
fewensa <fewensa@protonmail.com>
2022-06-28 17:06:00 +0800

    remove transactional

--- modules/fee-market/src/s2s/payment.rs
+++ modules/fee-market/src/s2s/payment.rs
@@ -26,7 +26,7 @@ use frame_support::{
 	traits::{Currency as CurrencyT, ExistenceRequirement, Get},
 };
 use scale_info::TypeInfo;
-use sp_runtime::traits::{AccountIdConversion, Saturating, Zero};
+use sp_runtime::traits::{Saturating, Zero};
 use sp_std::{
 	collections::{btree_map::BTreeMap, vec_deque::VecDeque},
 	ops::RangeInclusive,
