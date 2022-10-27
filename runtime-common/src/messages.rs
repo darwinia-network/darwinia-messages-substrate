2673e9c0938358d5ffeaa2c407e7d364847ebc1b
Guantong <04637@163.com>
2022-10-27 14:45:49 +0800

    Companion for paritytech/parity-bridges-common#1518

--- runtime-common/src/messages.rs
+++ runtime-common/src/messages.rs
@@ -46,6 +46,7 @@ use sp_runtime::{
 };
 use sp_std::prelude::*;
 use sp_trie::StorageProof;
+use xcm::latest::prelude::*;
 
 /// Bidirectional message bridge.
 pub trait MessageBridge {
@@ -145,6 +146,7 @@ pub type RawStorageProof = Vec<Vec<u8>>;
 
 /// Sub-module that is declaring types required for processing This -> Bridged chain messages.
 pub mod source {
+	use frame_support::log;
 	use super::*;
 
 	/// Encoded Call of the Bridged chain. We never try to decode it on This chain.
@@ -417,6 +419,89 @@ pub mod source {
 
 		Ok((lane, inbound_lane_data))
 	}
+
+	/// XCM bridge adapter for `bridge-messages` pallet.
+	pub struct XcmBridgeAdapter<T>(PhantomData<T>);
+
+	impl<T: XcmBridge> SendXcm for XcmBridgeAdapter<T>
+		where
+			BalanceOf<ThisChain<T::MessageBridge>>: Into<Fungibility>,
+			OriginOf<ThisChain<T::MessageBridge>>: From<pallet_xcm::Origin>,
+	{
+		type Ticket = (BalanceOf<ThisChain<T::MessageBridge>>, FromThisChainMessagePayload);
+
+		fn validate(
+			dest: &mut Option<MultiLocation>,
+			msg: &mut Option<Xcm<()>>,
+		) -> SendResult<Self::Ticket> {
+			let d = dest.take().ok_or(SendError::MissingArgument)?;
+			if !T::verify_destination(&d) {
+				*dest = Some(d);
+				return Err(SendError::NotApplicable)
+			}
+
+			let route = T::build_destination();
+			let msg = (route, msg.take().ok_or(SendError::MissingArgument)?).encode();
+
+			let fee = estimate_message_dispatch_and_delivery_fee::<T::MessageBridge>(
+				&msg,
+				T::MessageBridge::RELAYER_FEE_PERCENT,
+				None,
+			);
+			let fee = match fee {
+				Ok(fee) => fee,
+				Err(e) => {
+					log::trace!(
+						target: "runtime::bridge",
+						"Failed to comupte fee for XCM message to {:?}: {:?}",
+						T::MessageBridge::BRIDGED_CHAIN_ID,
+						e,
+					);
+					*dest = Some(d);
+					return Err(SendError::Transport(e))
+				},
+			};
+			let fee_assets = MultiAssets::from((Here, fee));
+
+			Ok(((fee, msg), fee_assets))
+		}
+
+		fn deliver(ticket: Self::Ticket) -> Result<XcmHash, SendError> {
+			use bp_messages::source_chain::MessagesBridge;
+
+			let lane = T::xcm_lane();
+			let (fee, msg) = ticket;
+			let result = T::MessageSender::send_message(
+				pallet_xcm::Origin::from(MultiLocation::from(T::universal_location())).into(),
+				lane,
+				msg,
+				fee,
+			);
+			result
+				.map(|artifacts| {
+					let hash = (lane, artifacts.nonce).using_encoded(sp_io::hashing::blake2_256);
+					log::debug!(
+						target: "runtime::bridge",
+						"Sent XCM message {:?}/{} to {:?}: {:?}",
+						lane,
+						artifacts.nonce,
+						T::MessageBridge::BRIDGED_CHAIN_ID,
+						hash,
+					);
+					hash
+				})
+				.map_err(|e| {
+					log::debug!(
+						target: "runtime::bridge",
+						"Failed to send XCM message over lane {:?} to {:?}: {:?}",
+						lane,
+						T::MessageBridge::BRIDGED_CHAIN_ID,
+						e,
+					);
+					SendError::Transport("Bridge has rejected the message")
+				})
+		}
+	}
 }
 
 /// Sub-module that is declaring types required for processing Bridged -> This chain messages.
@@ -554,8 +639,6 @@ pub mod target {
 			_relayer_account: &AccountIdOf<ThisChain<B>>,
 			message: DispatchMessage<Self::DispatchPayload, BalanceOf<BridgedChain<B>>>,
 		) -> MessageDispatchResult {
-			use xcm::latest::*;
-
 			let message_id = (message.key.lane_id, message.key.nonce);
 			let do_dispatch = move || -> sp_std::result::Result<Outcome, codec::Error> {
 				let FromBridgedChainMessagePayload { xcm: (location, xcm), weight: weight_limit } =
