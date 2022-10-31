55b9bf225618aaf4a62db44eb1abf74c56679068
bear <boundless.forest@outlook.com>
2022-11-01 07:57:34 +0800

    Integrate `ensure_bridge_integrity` (#221)

--- runtime-common/src/pallets.rs
+++ runtime-common/src/pallets.rs
@@ -28,15 +28,24 @@ pub const WITH_CRAB_MESSAGES_PALLET_NAME: &str = "BridgeCrabMessages";
 /// Name of the With-CrabParachain messages pallet instance that is deployed at bridged chains.
 pub const WITH_CRAB_PARACHAIN_MESSAGES_PALLET_NAME: &str = "BridgeCrabParachainMessages";
 
+/// Name of the With-Pangoro GRANDPA pallet instance that is deployed at bridged chains.
+pub const WITH_PANGORO_GRANDPA_PALLET_NAME: &str = "BridgePangoroGrandpa";
 /// Name of the With-Pangoro messages pallet instance that is deployed at bridged chains.
 pub const WITH_PANGORO_MESSAGES_PALLET_NAME: &str = "BridgePangoroMessages";
 
+/// Name of the With-Pangolin GRANDPA pallet instance that is deployed at bridged chains.
+pub const WITH_PANGOLIN_GRANDPA_PALLET_NAME: &str = "BridgePangolinGrandpa";
 /// Name of the With-Pangolin messages pallet instance that is deployed at bridged chains.
 pub const WITH_PANGOLIN_MESSAGES_PALLET_NAME: &str = "BridgePangolinMessages";
 
+/// Name of the With-PangolinParachain GRANDPA pallet instance that is deployed at bridged chains.
+pub const WITH_PANGOLIN_PARACHAIN_GRANDPA_PALLET_NAME: &str = "BridgeRococoGrandpa";
 /// Name of the With-PangolinParachain messages pallet instance that is deployed at bridged chains.
 pub const WITH_PANGOLIN_PARACHAIN_MESSAGES_PALLET_NAME: &str = "BridgePangolinParachainMessages";
 
+/// Name of the With-PangolinParachainAlpha GRANDPA pallet instance that is deployed at bridged
+/// chains.
+pub const WITH_PANGOLIN_PARACHAIN_ALPHA_GRANDPA_PALLET_NAME: &str = "BridgeMoonbaseRelayGrandpa";
 /// Name of the With-PangolinParachainAlpha messages pallet instance that is deployed at bridged
 /// chains.
 pub const WITH_PANGOLIN_PARACHAIN_ALPHA_MESSAGES_PALLET_NAME: &str =
