c6bb1cf64af84cddd992524598847b8eb76f4e21
bear <boundless.forest@outlook.com>
2023-04-07 13:53:37 +0800

    Delete useless config and update `DARWINIA_CRAB_LANE` (#262)

--- runtime-common/src/lanes.rs
+++ runtime-common/src/lanes.rs
@@ -20,16 +20,7 @@
 use bp_messages::LaneId;
 
 /// Identifier of bridge between Darwinia and Crab.
-pub const DARWINIA_CRAB_LANE: LaneId = [0; 4];
+pub const DARWINIA_CRAB_LANE: LaneId = *b"dacb";
 
 // Identifier of bridge between Pangoro and Pangolin.
 pub const PANGORO_PANGOLIN_LANE: LaneId = *b"roli";
-
-/// Identifier of bridge between Pangolin and Pangolin Parachain.
-pub const PANGOLIN_PANGOLIN_PARACHAIN_LANE: LaneId = *b"pali";
-
-/// Identifier of bridge between Pangolin and Pangolin Parachain Alpha.
-pub const PANGOLIN_PANGOLIN_PARACHAIN_ALPHA_LANE: LaneId = *b"plpa";
-
-/// Identifier of bridge between Crab and Crab Parachain.
-pub const CRAB_CRAB_PARACHAIN_LANE: LaneId = *b"pacr";
